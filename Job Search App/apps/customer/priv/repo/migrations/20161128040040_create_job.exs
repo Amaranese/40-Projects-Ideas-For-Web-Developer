817266cdc8bf63cdebf5d0a0f5bfad91ea6b8529
Amaranese <alejandromaranes@gmail.com>
2022-06-06 10:45:50 +0200

    Create 20161128040040_create_job.exs

---
+++ Job Search App/apps/customer/priv/repo/migrations/20161128040040_create_job.exs
@@ -0,0 +1,20 @@
+defmodule Customer.Repo.Migrations.CreateJob do
+  use Ecto.Migration
+
+  def change do
+    create table(:jobs) do
+      add :company_id, references(:companies, on_delete: :delete_all), null: false
+      add :area_id, references(:areas, on_delete: :nilify_all), null: false
+      add :job_title_id, references(:job_titles, on_delete: :nilify_all), null: false
+      add :url, :map, null: false
+      add :title, :map, null: false
+      add :detail, :map
+
+      timestamps()
+    end
+    create index(:jobs, :company_id)
+    create index(:jobs, :area_id)
+    create index(:jobs, :job_title_id)
+
+  end
+end
