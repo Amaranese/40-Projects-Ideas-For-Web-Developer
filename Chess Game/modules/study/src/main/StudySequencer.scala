4de6fb9cd62d693ec2c5cb4af2fbd89c56dbec7a
Amaranese <alejandromaranes@gmail.com>
2022-06-20 11:26:27 +0200

    Create StudySequencer.scala

---
+++ Chess Game/modules/study/src/main/StudySequencer.scala
@@ -0,0 +1,40 @@
+package lila.study
+
+import alleycats.Zero
+import scala.concurrent.duration._
+
+import lila.hub.AsyncActorSequencers
+
+final private class StudySequencer(
+    studyRepo: StudyRepo,
+    chapterRepo: ChapterRepo
+)(implicit
+    ec: scala.concurrent.ExecutionContext,
+    system: akka.actor.ActorSystem,
+    mode: play.api.Mode
+) {
+
+  private val workQueue =
+    new AsyncActorSequencers(maxSize = 64, expiration = 1 minute, timeout = 10 seconds, name = "study")
+
+  def sequenceStudy[A: Zero](studyId: Study.Id)(f: Study => Fu[A]): Fu[A] =
+    workQueue(studyId.value) {
+      studyRepo.byId(studyId) flatMap {
+        _ ?? { f(_) }
+      }
+    }
+
+  def sequenceStudyWithChapter[A: Zero](studyId: Study.Id, chapterId: Chapter.Id)(
+      f: Study.WithChapter => Fu[A]
+  ): Fu[A] =
+    sequenceStudy(studyId) { study =>
+      chapterRepo
+        .byId(chapterId)
+        .flatMap {
+          _.filter(_.studyId == studyId) ?? { chapter =>
+            f(Study.WithChapter(study, chapter))
+          }
+        }
+        .mon(_.study.sequencer.chapterTime)
+    }
+}
