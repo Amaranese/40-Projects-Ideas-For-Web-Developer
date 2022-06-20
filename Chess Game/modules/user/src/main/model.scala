d4c50d38ae1250d1ac9f5bde922c7696fe9937dd
Amaranese <alejandromaranes@gmail.com>
2022-06-20 11:31:09 +0200

    Create model.scala

---
+++ Chess Game/modules/user/src/main/model.scala
@@ -0,0 +1,14 @@
+package lila.user
+
+final class GetBotIds(f: () => Fu[Set[User.ID]]) extends (() => Fu[Set[User.ID]]) {
+  def apply() = f()
+}
+
+final class RankingsOf(f: User.ID => lila.rating.UserRankMap) extends (User.ID => lila.rating.UserRankMap) {
+  def apply(u: User.ID) = f(u)
+}
+
+// permission holder
+case class Holder(user: User) extends AnyVal {
+  def id = user.id
+}
