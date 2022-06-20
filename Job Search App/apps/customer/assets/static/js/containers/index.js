cfe5e28eb103ba99912aeb43412c43bd9a7f568f
Amaranese <alejandromaranes@gmail.com>
2022-06-20 11:45:52 +0200

    Create index.js

---
+++ Job Search App/apps/customer/assets/static/js/containers/index.js
@@ -0,0 +1,6 @@
+export HeaderContainer from './HeaderContainer';
+export AuthenticationContainer from "./AuthenticationContainer";
+export JobIndexContainer from "./JobIndexContainer";
+export JobShowContainer from "./JobShowContainer";
+export FavoriteJobIndexContainer from "./FavoriteJobIndexContainer";
+export App from "./App";
