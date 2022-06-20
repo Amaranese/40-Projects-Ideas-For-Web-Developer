f6aff26dc165126ef1d8f63a3f11f6590e0913e1
Amaranese <alejandromaranes@gmail.com>
2022-06-20 11:45:27 +0200

    Create constants.js

---
+++ Job Search App/apps/customer/assets/static/js/containers/FavoriteJobIndexContainer/constants.js
@@ -0,0 +1,3 @@
+import { createRequestTypes } from 'constants';
+export const FETCH_FAVORITE_JOBS = createRequestTypes('fetch_favorite_jobs');
+export const UPDATE_FAVORITE_JOB = createRequestTypes('update_favorite_job');
