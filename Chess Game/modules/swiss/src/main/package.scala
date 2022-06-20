6a79df251b51393363f7f62c1465fc41462d3330
Amaranese <alejandromaranes@gmail.com>
2022-06-20 11:26:50 +0200

    bv

---
+++ Chess Game/modules/swiss/src/main/package.scala
@@ -0,0 +1,16 @@
+package lila
+
+import lila.user.User
+
+package object swiss extends PackageObject {
+
+  val lichessTeamId = "lichess-swiss"
+
+  type Ranking = Map[lila.user.User.ID, Int]
+
+  private[swiss] val logger = lila.log("swiss")
+
+  // FIDE TRF player IDs
+  private[swiss] type PlayerIds = Map[User.ID, Int]
+  private[swiss] type IdPlayers = Map[Int, User.ID]
+}
